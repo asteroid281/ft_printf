bbf8a9ff34a6d2d6fa993138a18baf1d97f5cb07
asteroid281 <109768680+asteroid281@users.noreply.github.com>
2024-02-21 17:22:24 +0300

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yunozdem <yunozdem@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 16:19:12 by yunozdem          #+#    #+#             */
+/*   Updated: 2024/02/21 16:31:26 by yunozdem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "./libft/libft.h"
+
+int		ft_printf(const char *input, ...);
+int		print_pointer(unsigned long value, int asc);
+int		print_unsigned(unsigned int nb);
+int		print_string(char *s);
+int		print_int(int n);
+int		print_char(char c);
+int		print_hex(unsigned int value, int asc);
+
+#endif
